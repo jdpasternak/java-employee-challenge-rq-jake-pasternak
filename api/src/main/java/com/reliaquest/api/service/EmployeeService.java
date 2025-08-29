aeca9441def4f05d6b657ab6ba97890f9ea6fad2
Jake Pasternak <jdp.pasternak@gmail.com>
2025-08-28 23:53:46 -0400

    feat(service): implement EmployeeService::search with validation.

--- api/src/main/java/com/reliaquest/api/service/EmployeeService.java
+++ api/src/main/java/com/reliaquest/api/service/EmployeeService.java
@@ -2,16 +2,22 @@ package com.reliaquest.api.service;
 
 import com.reliaquest.api.exception.DownstreamUnavailableException;
 import com.reliaquest.api.exception.EmployeeNotFoundException;
-import com.reliaquest.api.exception.ValidationException;
 import com.reliaquest.api.gateway.EmployeeClient;
 import com.reliaquest.api.model.CreateEmployeeInput;
 import com.reliaquest.api.model.Employee;
-import java.util.List;
-import java.util.Optional;
+import com.reliaquest.api.model.SearchInput;
+import jakarta.validation.Valid;
+import jakarta.validation.ValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
 
 @Service
+@Validated
 public class EmployeeService {
 
     @Autowired
@@ -21,8 +27,13 @@ public class EmployeeService {
         return client.getAll();
     }
 
-    public List<Employee> search(String fragment) throws ValidationException {
-        return null;
+    public List<Employee> search(@Valid SearchInput searchInput) throws DownstreamUnavailableException {
+        List<Employee> employees = client.getAll();
+        String normalizedSearchString = searchInput.getSearchString().toLowerCase(Locale.ROOT);
+        return employees.stream().filter(employee ->
+                        employee.getName().toLowerCase(Locale.ROOT)
+                                .contains(normalizedSearchString))
+                .toList();
     }
 
     public Employee findById(String id) throws EmployeeNotFoundException {
