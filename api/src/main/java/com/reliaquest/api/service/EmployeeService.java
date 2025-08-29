f64a4d8d6e9a2d530c4337b3ef2778dbd99080b8
Jake Pasternak <jdp.pasternak@gmail.com>
2025-08-28 20:08:26 -0400

    feat(service): create EmployeeService skeleton, all methods return null, throw exceptions where needed.

---
+++ api/src/main/java/com/reliaquest/api/service/EmployeeService.java
@@ -0,0 +1,41 @@
+package com.reliaquest.api.service;
+
+import com.reliaquest.api.exception.EmployeeNotFoundException;
+import com.reliaquest.api.exception.ValidationException;
+import com.reliaquest.api.model.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeService {
+
+    public List<Employee> findAll() {
+        return null;
+    }
+
+    public List<Employee> search(String fragment) throws ValidationException {
+        return null;
+    }
+
+    public Employee findById(String id) throws EmployeeNotFoundException {
+        return null;
+    }
+
+    public Optional<Integer> findHighestSalaryOfEmployees() {
+        return null;
+    }
+
+    public List<Employee> findTopTenHighestEarningEmployees() {
+        return null;
+    }
+
+    public Employee createEmployee(Employee employeeInput) throws ValidationException {
+        return null;
+    }
+
+    public boolean deleteEmployeeById(String id) throws EmployeeNotFoundException {
+        return false;
+    }
+}
