4c6c950faa1a4b24460ec4c57ec65603f9aef4f1
Jake Pasternak <jdp.pasternak@gmail.com>
2025-08-29 18:43:10 -0400

    feat(service): add employee comparator
    - required for findTopTenHighestEarningEmployees

--- api/src/main/java/com/reliaquest/api/service/EmployeeService.java
+++ api/src/main/java/com/reliaquest/api/service/EmployeeService.java
@@ -2,6 +2,7 @@ package com.reliaquest.api.service;
 
 import com.reliaquest.api.exception.EmployeeNotFoundException;
 import com.reliaquest.api.gateway.EmployeeClient;
+import com.reliaquest.api.helper.EmployeeComparators;
 import com.reliaquest.api.model.CreateEmployeeInput;
 import com.reliaquest.api.model.Employee;
 import com.reliaquest.api.model.SearchInput;
