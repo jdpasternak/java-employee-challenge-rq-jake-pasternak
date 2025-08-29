b301396057b2b37f22537649292fefddf6e3c44d
Jake Pasternak <jdp.pasternak@gmail.com>
2025-08-28 20:41:16 -0400

    feat(api): add EmployeeControllerAdvice to handle exceptions during requests.

---
+++ api/src/main/java/com/reliaquest/api/controller/EmployeeControllerAdvice.java
@@ -0,0 +1,19 @@
+package com.reliaquest.api.controller;
+
+import com.reliaquest.api.model.Response;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Slf4j
+@ControllerAdvice
+public class EmployeeControllerAdvice {
+
+    @ExceptionHandler
+    protected ResponseEntity<?> handleException(Throwable ex) {
+        log.error("Error handling web request.", ex);
+        // TODO create response model
+        return ResponseEntity.internalServerError().body(Response.error(ex.getMessage()));
+    }
+}
