77a91ed4feab521e225aa7ed30964cfe4efe2997
Jake Pasternak <jdp.pasternak@gmail.com>
2025-08-29 15:57:27 -0400

    feat(api): switch CreateEmployeeInput to record with bean validation
     Record is immutable
     Trim name and title in constructor

--- api/src/main/java/com/reliaquest/api/model/CreateEmployeeInput.java
+++ api/src/main/java/com/reliaquest/api/model/CreateEmployeeInput.java
@@ -3,20 +3,17 @@ package com.reliaquest.api.model;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Data;
+import jakarta.validation.constraints.NotNull;
 
-@Data
-public class CreateEmployeeInput {
-    @NotBlank
-    private String name;
 
-    @Min(1)
-    private int salary;
-
-    @Min(16)
-    @Max(75)
-    private int age;
-
-    @NotBlank
-    private String title;
+public record CreateEmployeeInput(
+        @NotBlank String name,
+        @NotNull @Min(1) Integer salary,
+        @NotNull @Min(16) @Max(75) Integer age,
+        @NotBlank String title
+) {
+    public CreateEmployeeInput {
+        name = name == null ? null : name.trim();
+        title = title == null ? null : title.trim();
+    }
 }
