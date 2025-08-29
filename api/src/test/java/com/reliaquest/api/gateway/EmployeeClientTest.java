2d69d5f46ff3ec1c38502f69baaf3ace1f4db2f6
Jake Pasternak <jdp.pasternak@gmail.com>
2025-08-29 19:17:53 -0400

    test(client): sets up EmployeeClientTest

---
+++ api/src/test/java/com/reliaquest/api/gateway/EmployeeClientTest.java
@@ -0,0 +1,29 @@
+package com.reliaquest.api.gateway;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeClientTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void deleteByName() {
+    }
+}
