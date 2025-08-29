e253d05ffb7bab3e07b9f93b06669991956077aa
Jake Pasternak <jdp.pasternak@gmail.com>
2025-08-29 17:00:28 -0400

    test(service): optimize imports

--- api/src/test/java/com/reliaquest/api/service/EmployeeServiceTest.java
+++ api/src/test/java/com/reliaquest/api/service/EmployeeServiceTest.java
@@ -2,7 +2,6 @@ package com.reliaquest.api.service;
 
 import com.reliaquest.api.exception.DownstreamUnavailableException;
 import com.reliaquest.api.exception.EmployeeNotFoundException;
-import com.reliaquest.api.exception.ValidationException;
 import com.reliaquest.api.gateway.EmployeeClient;
 import com.reliaquest.api.model.CreateEmployeeInput;
 import com.reliaquest.api.model.Employee;
