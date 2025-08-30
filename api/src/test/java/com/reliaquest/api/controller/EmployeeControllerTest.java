d05dabe94b41147574b4222323a03fd39a16b46a
Jake Pasternak <jdp.pasternak@gmail.com>
2025-08-30 14:38:55 -0400

    test(controller): add EmployeeControllerTest skeleton

---
+++ api/src/test/java/com/reliaquest/api/controller/EmployeeControllerTest.java
@@ -0,0 +1,40 @@
+package com.reliaquest.api.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class EmployeeControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAllEmployees() {
+    }
+
+    @Test
+    void getEmployeesByNameSearch() {
+    }
+
+    @Test
+    void getEmployeeById() {
+    }
+
+    @Test
+    void getHighestSalaryOfEmployees() {
+    }
+
+    @Test
+    void getTopTenHighestEarningEmployeeNames() {
+    }
+
+    @Test
+    void createEmployee() {
+    }
+
+    @Test
+    void deleteEmployeeById() {
+    }
+}
