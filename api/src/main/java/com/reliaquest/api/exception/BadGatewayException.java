73c512d444c0f2d78b431a7a6f607ca50ec94c3b
Jake Pasternak <jdp.pasternak@gmail.com>
2025-08-29 22:35:12 -0400

    feat(domain): add cause BadGatewayException
    - property Exception cause
    - overloads constructor to take cause, type Exception

--- api/src/main/java/com/reliaquest/api/exception/BadGatewayException.java
+++ api/src/main/java/com/reliaquest/api/exception/BadGatewayException.java
@@ -1,6 +1,7 @@
 package com.reliaquest.api.exception;
 
 public class BadGatewayException extends RuntimeException {
+    private Exception cause;
 
     public BadGatewayException() {
         super("bad gateway!");
@@ -8,5 +9,10 @@ public class BadGatewayException extends RuntimeException {
 
     public BadGatewayException(String message) {
         super(message);
+    }
+
+    public BadGatewayException(String message, Exception cause) {
+        super(message);
+        this.cause = cause;
     }
 }
