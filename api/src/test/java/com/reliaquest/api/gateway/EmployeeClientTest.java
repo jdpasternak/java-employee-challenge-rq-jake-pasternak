7a9dc6b4900feb47f6034c6e8920880eb1db773b
Jake Pasternak <jdp.pasternak@gmail.com>
2025-08-29 20:18:18 -0400

    test(api): add getAll test
    - response body has list of two employees
    - assertions on the list and names included

--- api/src/test/java/com/reliaquest/api/gateway/EmployeeClientTest.java
+++ api/src/test/java/com/reliaquest/api/gateway/EmployeeClientTest.java
@@ -32,7 +32,43 @@ class EmployeeClientTest {
     }
 
     @Test
-    void getAll() {
+    void getAll_whenEmployeesExist_returnsListOfEmployees() {
+        // Given
+        String body = """
+                { "data": [
+                      {
+                        "id":"%s",
+                        "employee_name":"Bill Bob",
+                        "employee_salary":89750,
+                        "employee_age":24,
+                        "employee_title":"Documentation Engineer",
+                        "employee_email":"billBob@company.com"
+                      },
+                      {
+                        "id":"%s",
+                        "employee_name":"Sally Sue",
+                        "employee_salary":12345,
+                        "employee_age":28,
+                        "employee_title":"Documentation Manager",
+                        "employee_email":"sallysue@company.com"
+                      },
+                  ],
+                  "status":"Successfully processed request."
+                }""".formatted(UUID.randomUUID(), UUID.randomUUID());
+
+        server.expect(MockRestRequestMatchers.requestTo("http://localhost:8112/api/v1/employee"))
+                .andExpect(MockRestRequestMatchers.method(HttpMethod.GET))
+                .andRespond(MockRestResponseCreators.withSuccess(body, MediaType.APPLICATION_JSON));
+
+        // When
+        List<Employee> employees = client.getAll();
+
+        // Then
+        Assertions.assertNotNull(employees);
+        Assertions.assertFalse(employees.isEmpty());
+        Assertions.assertEquals(2, employees.size());
+        Assertions.assertTrue(employees.stream().map(Employee::getName).toList()
+                .containsAll(List.of("Bill Bob", "Sally Sue")));
     }
 
     @Test
