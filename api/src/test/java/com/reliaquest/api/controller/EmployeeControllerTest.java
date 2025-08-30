bd8a619347e24f730dbba5c2d55dc721dd2294b8
Jake Pasternak <jdp.pasternak@gmail.com>
2025-08-30 15:06:52 -0400

    test(controller): enhance EmployeeControllerTest
    - add annotations
    - setup dependencies, mocks

--- api/src/test/java/com/reliaquest/api/controller/EmployeeControllerTest.java
+++ api/src/test/java/com/reliaquest/api/controller/EmployeeControllerTest.java
@@ -1,13 +1,32 @@
 package com.reliaquest.api.controller;
 
+import com.reliaquest.api.model.Employee;
+import com.reliaquest.api.service.EmployeeService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+import java.util.UUID;
 
 
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class EmployeeControllerTest {
+    EmployeeController controller;
+
+    @Mock
+    EmployeeService service;
 
     @BeforeEach
     void setUp() {
+        controller = new EmployeeController(service);
     }
 
     @Test
