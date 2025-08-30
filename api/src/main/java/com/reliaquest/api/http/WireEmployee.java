ac3d39c88694323c156142173f8b5af240b4d609
Jake Pasternak <jdp.pasternak@gmail.com>
2025-08-29 20:23:19 -0400

    feat(api): add WireEmployee to handle mock api employee response body
    - annotate with JsonProperty to map to mock API server response

---
+++ api/src/main/java/com/reliaquest/api/http/WireEmployee.java
@@ -0,0 +1,12 @@
+package com.reliaquest.api.http;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record WireEmployee(
+        String id,
+        @JsonProperty("employee_name") String employeeName,
+        @JsonProperty("employee_salary") Integer employeeSalary,
+        @JsonProperty("employee_age") Integer employeeAge,
+        @JsonProperty("employee_title") String employeeTitle,
+        @JsonProperty("employee_email") String employeeEmail
+) {}
