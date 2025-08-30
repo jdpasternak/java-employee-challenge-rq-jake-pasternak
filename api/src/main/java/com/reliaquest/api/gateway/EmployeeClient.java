2333c202f4f040fbc42c5778d621ab28e4480ed6
Jake Pasternak <jdp.pasternak@gmail.com>
2025-08-29 21:41:34 -0400

    feat(api): add implementation for EmployeeClient::getAll
    - passes happy path, empty data, and server error tests

--- api/src/main/java/com/reliaquest/api/gateway/EmployeeClient.java
+++ api/src/main/java/com/reliaquest/api/gateway/EmployeeClient.java
@@ -1,20 +1,43 @@
 package com.reliaquest.api.gateway;
 
+import com.reliaquest.api.exception.EmployeeNotFoundException;
+import com.reliaquest.api.http.EmployeeMapper;
+import com.reliaquest.api.http.Envelope;
+import com.reliaquest.api.http.WireEmployee;
 import com.reliaquest.api.model.CreateEmployeeInput;
 import com.reliaquest.api.model.Employee;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Component
-public class EmployeeClient {
+public class EmployeeClient implements IEmployeeClient {
 
-    public List<Employee> getAll() {
-        return null;
+    private final RestTemplate restTemplate;
+
+    public EmployeeClient(@Qualifier("employeeRestTemplate") RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 
-    public Employee getById(UUID id) {
+    public List<Employee> getAll() {
+        var type = new ParameterizedTypeReference<Envelope<List<WireEmployee>>>() {
+        };
+        var response = restTemplate.exchange("/employee", HttpMethod.GET, null, type);
+
+        var wire = Optional.ofNullable(response.getBody())
+                .map(Envelope::data)
+                .orElse(List.of());
+
+        return wire.stream().map(EmployeeMapper::toDomain).toList();
+    }
+
+    public Employee getById(UUID id) throws EmployeeNotFoundException {
         return null;
     }
 
@@ -22,7 +45,7 @@ public class EmployeeClient {
         return null;
     }
 
-    public boolean deleteByName(String name) {
+    public boolean deleteByName(String name) throws EmployeeNotFoundException {
         return false;
     }
 }
