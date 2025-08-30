8da5d75c950a7773626011ea2f237614059eefd3
Jake Pasternak <jdp.pasternak@gmail.com>
2025-08-30 16:20:19 -0400

    feat(controller): enhance EmployeeController
    - removes RequiredArgsConstructor since we are injecting via constructor
    - make EmployeeService private final

--- api/src/main/java/com/reliaquest/api/controller/EmployeeController.java
+++ api/src/main/java/com/reliaquest/api/controller/EmployeeController.java
@@ -3,7 +3,6 @@ package com.reliaquest.api.controller;
 import com.reliaquest.api.model.CreateEmployeeInput;
 import com.reliaquest.api.model.Employee;
 import com.reliaquest.api.service.EmployeeService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +11,9 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/employee")
-@RequiredArgsConstructor
 public class EmployeeController implements IEmployeeController<Employee, CreateEmployeeInput> {
 
-    EmployeeService employeeService;
+    private final EmployeeService employeeService;
 
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
