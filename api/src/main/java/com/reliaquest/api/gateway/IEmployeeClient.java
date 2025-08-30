0719706b9b709f63d11b9e6db48623dbe771620c
Jake Pasternak <jdp.pasternak@gmail.com>
2025-08-30 09:38:46 -0400

    feat(client): add validation to IEmployeeClient
    - interface now specifies validation
    - enables testing

--- api/src/main/java/com/reliaquest/api/gateway/IEmployeeClient.java
+++ api/src/main/java/com/reliaquest/api/gateway/IEmployeeClient.java
@@ -2,13 +2,17 @@ package com.reliaquest.api.gateway;
 
 import com.reliaquest.api.model.CreateEmployeeInput;
 import com.reliaquest.api.model.Employee;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
 import java.util.UUID;
 
+@Validated
 public interface IEmployeeClient {
     List<Employee> getAll();
-    Employee getById(UUID id);
-    Employee create(CreateEmployeeInput input);
-    boolean deleteByName(String name);
+    Employee getById(@NotNull UUID id);
+    Employee create(@Valid @NotNull CreateEmployeeInput input);
+    boolean deleteByName(@NotNull String name);
 }
