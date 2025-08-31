f6dd88bb3ef03fe44daaa9b081229027d731cb92
Jake Pasternak <jdp.pasternak@gmail.com>
2025-08-31 15:02:59 -0400

    refactor(api): use HttpStatus constants for error codes

--- api/src/main/java/com/reliaquest/api/http/DownstreamErrorHandler.java
+++ api/src/main/java/com/reliaquest/api/http/DownstreamErrorHandler.java
@@ -3,24 +3,25 @@ package com.reliaquest.api.http;
 import com.reliaquest.api.exception.BadGatewayException;
 import com.reliaquest.api.exception.DownstreamUnavailableException;
 import com.reliaquest.api.exception.EmployeeNotFoundException;
-import java.io.IOException;
-import java.time.Duration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.DefaultResponseErrorHandler;
 
+import java.io.IOException;
+import java.time.Duration;
+
 public class DownstreamErrorHandler extends DefaultResponseErrorHandler {
     @Override
     public void handleError(ClientHttpResponse response) throws IOException {
         HttpStatusCode status = response.getStatusCode();
 
-        if (status.value() == 404) {
+        if (status.value() == HttpStatus.NOT_FOUND.value()) {
             throw new EmployeeNotFoundException();
         }
 
-        if (status.value() == 429) {
+        if (status.value() == HttpStatus.TOO_MANY_REQUESTS.value()) {
             Duration retryAfter = parseRetryAfter(response.getHeaders().getFirst(HttpHeaders.RETRY_AFTER));
             throw new DownstreamUnavailableException("rate limited", retryAfter);
         }
