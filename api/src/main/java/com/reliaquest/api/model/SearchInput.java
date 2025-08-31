3f777c7a7fc764fc8f0c4baea91727c8ccab75df
Jake Pasternak <jdp.pasternak@gmail.com>
2025-08-31 15:05:37 -0400

    refactor(domain): add AllArgsConstructor to SearchInput

--- api/src/main/java/com/reliaquest/api/model/SearchInput.java
+++ api/src/main/java/com/reliaquest/api/model/SearchInput.java
@@ -1,9 +1,11 @@
 package com.reliaquest.api.model;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class SearchInput {
     @NotBlank
     private String searchString;
