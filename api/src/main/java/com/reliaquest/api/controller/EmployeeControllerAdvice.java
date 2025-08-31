502115afef06ee543c40c2ebb01a9cf5086cea16
Jake Pasternak <jdp.pasternak@gmail.com>
2025-08-30 23:30:02 -0400

    feat(controller): add handler for NoResourceFoundException
    - return status 405 if any non-existent resource use is attempted

--- api/src/main/java/com/reliaquest/api/controller/EmployeeControllerAdvice.java
+++ api/src/main/java/com/reliaquest/api/controller/EmployeeControllerAdvice.java
@@ -4,9 +4,11 @@ import com.reliaquest.api.exception.EmployeeNotFoundException;
 import com.reliaquest.api.exception.EmployeeWithNameAlreadyExistsException;
 import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @Slf4j
 @ControllerAdvice
@@ -29,5 +31,11 @@ public class EmployeeControllerAdvice {
     protected ResponseEntity<?> handleEmployeeNotFoundException(Throwable ex) {
         log.error("EmployeeNotFoundException!", ex);
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    protected ResponseEntity<?> handleNoResourceFoundException(Throwable ex) {
+        log.error("NoResourceFoundException!", ex);
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).build();
     }
 }
