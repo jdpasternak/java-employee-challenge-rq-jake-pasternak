5dc3978bc9422a3ed29d0ecda7209336aaaa6e96
Jake Pasternak <jdp.pasternak@gmail.com>
2025-08-31 18:35:19 -0400

    feat(domain): enhance EmployeeNotFoundException to take id property
    - for better logging and user feedback

--- api/src/main/java/com/reliaquest/api/exception/EmployeeNotFoundException.java
+++ api/src/main/java/com/reliaquest/api/exception/EmployeeNotFoundException.java
@@ -1,12 +1,20 @@
 package com.reliaquest.api.exception;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class EmployeeNotFoundException extends RuntimeException {
+
+    private String id;
 
     public EmployeeNotFoundException() {
         super("Employee not found!");
     }
 
-    public EmployeeNotFoundException(String message) {
-        super(message);
+    public EmployeeNotFoundException(String id) {
+        this();
+        this.id = id;
     }
 }
